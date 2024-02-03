c49f987a6505d2d6641762d5e2930602e764b207
signux <signux@gmx.net>
2024-02-03 14:29:07 +0100

    add nextcloud app template

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: Chris Schmidt <signux@gmx.net>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+module.exports = {
+	extends: [
+		'@nextcloud',
+	]
+}
---
+++ lib/Service/NoteNotFound.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Chris Schmidt <signux@gmx.net>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\Haushaltsbuch\Service;
+
+class NoteNotFound extends \Exception {
+}
